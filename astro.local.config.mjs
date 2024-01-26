2fd6a563ef6cc723c923a5a094f1045e09eb1c96
Alexander Sabanov <alexsab@mail.ru>
2024-01-26 22:33:14 +0400

    Update astro.local.config.mjs

--- astro.local.config.mjs
+++ astro.local.config.mjs
@@ -9,7 +9,7 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
 	integrations: [
 		tailwind({
-			configFile: './tailwind.config.js'
+			configFile: './tailwind.geely.js'
 		}),
 		sitemap(),
 		robots(),
