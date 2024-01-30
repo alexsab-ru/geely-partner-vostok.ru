3abe2ba591ac1fdf9555b52c16a78aed2fe8de54
Alexander Sabanov <alexsab@mail.ru>
2024-01-30 23:14:22 +0400

    upd recaptcha action

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ const lightbox = GLightbox({
 });
 
 grecaptcha.ready(function() {
-	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'geely-partner-vostok.ru'}).then(function(token) {
+	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'geely_partner_vostok'}).then(function(token) {
 		let formData = new FormData();
 		formData.append('g-recaptcha-response', token);
 		const params = new URLSearchParams([...formData]);
